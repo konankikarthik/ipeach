c8c540d556d7efa648d4d9c65fda622db5084add
01101011karthik <o11o1o11.o11o1o11@gmail.com>
2022-05-09 20:45:08 +0530

    added initial ipeach commit --change this later--

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/atoms/NavBar";
+import Home from "./components/organisms/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
